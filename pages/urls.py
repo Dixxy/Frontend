42a7595667dbbd0bc15d03afb6c8f6392d095dd1
Dixxy <biloldixxy@gmail.com>
2021-11-02 15:26:47 +0500

    'fronetend'

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from pages.views import HomeView, AboutView
+from pages.views import HomeView, AboutView, ContactView, TravelView, DestinationView, BlogView, ElementView, \
+    SingleBlogView
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('travel/', TravelView.as_view(), name='travel'),
+    path('destination/', DestinationView.as_view(), name='destination'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('element/', ElementView.as_view(), name='element'),
+    path('single/', SingleBlogView.as_view(), name='single')
 ]
+
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,27 @@ class HomeView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
+class TravelView(TemplateView):
+    template_name = 'travel_destination.html'
+
+
+class DestinationView(TemplateView):
+    template_name = 'destination_details.html'
+
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
+
+class ElementView(TemplateView):
+    template_name = 'elements.html'
+
+
+class SingleBlogView(TemplateView):
+    template_name = 'single-blog.html'
